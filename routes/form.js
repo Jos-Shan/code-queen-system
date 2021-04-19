9e475017a88063c7be0302a997cbc48e8175955b
Jos-Shan <jsbabirye15@gmail.com>
2021-04-19 12:24:52 +0200

    responsivesness

--- routes/form.js
+++ routes/form.js
@@ -2,7 +2,7 @@ const { application } = require('express');
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Application = require('../models/Application'); 
+var Application = require('../models/Studentmodel'); 
 
 var app = express();
 
