ade8e9b8eda8aec91179347b38d3644a998b5d20
Jos-Shan <jsbabirye15@gmail.com>
2021-06-03 07:48:32 +0300

    student model

--- models/Studentsmodel.js
+++ models/Studentsmodel.js
@@ -1,14 +1,8 @@
 var mongoose = require('mongoose');
 
-
 const schema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-    },
+    email: { type: String, required: true, unique: true, lowercase: true },
     phone: { type: String, required: true, trim: true },
     birthdate: { type: Date, required: true, trim: true },
     age: { type: Number, required: true, trim: true },
@@ -22,4 +16,36 @@ const schema = new mongoose.Schema({
 });
 
 const Student = module.exports = mongoose.model("Student", schema);
+
+//nesting the schemas
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const AssessmentsSchema = new Schema({
+    HTML: { type: Number, required: true, trim: true },
+    CSS: { type: Number, required: true, trim: true },
+    JavaScript: { type: Number, required: true, trim: true },
+    Facilitator_rating: { type: String, required: true, trim: true },
+    hackathon: { type: String, required: true, trim: true },
+    title: String,
+    pages: Number
+});
+
+const StudentSchema = new Schema({
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    phone: { type: String, required: true, trim: true },
+    birthdate: { type: Date, required: true, trim: true },
+    age: { type: Number, required: true, trim: true },
+    cohort: { type: String, required: true, trim: true },
+    status: { type: String, required: true, trim: true },
+    assessment: [AssessmentsSchema]
+});
+
+const Student = mongoose.model('Student', StudentSchema);
+
+module.exports = Student;
+
+
 
---
+++ routes/students.js
@@ -0,0 +1,63 @@
+//const { application } = require('express');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Application = require('../models/Studentsmodel'); 
+
+var app = express();
+
+/* GET form page. */
+router.get('/', function (req, res, next) {
+   res.render('form')
+}); 
+
+
+router.post('/', function (req, res, next) {
+   var newApplication = new Application(req.body);
+   console.log("Data>>>>>",newApplication)
+   newApplication.save()
+   .then(data => {
+      console.log(data)
+      res.render('form', {message: "Application submitted"})
+      
+   })
+   .catch(err => {
+      console.log(err)
+      res.status(400).send("Unable to save to database")
+   });
+   
+}); 
+
+     //retrieving
+    app.get('/form', function(req, res){
+    Application.find(function(err, response){
+        console.log(response);
+     });
+    });
+    
+     //updating
+router.put('/:id', function(req, res) {
+Application.findByIdAndUpdate(req.params.id, req.body)
+   .then(application => {
+      console.log(application)
+      res.send("Updated successfully");
+   })
+   .catch(err => {
+      console.log(err)
+      res.status(400).send("Unable to update database")
+   });
+}); 
+
+    
+     //deleting
+router.delete('/:id', function(req, res){
+Application.findByIdAndRemove(req.params.id, function(err, response){
+        if(err) res.json({message: "Error in deleting record id " + req.params.id});
+        else res.json({message: "Person with id " + req.params.id + " removed."});
+     });
+    });
+    
+    
+
+module.exports = router;
+
